cf59c0680f094fe9af921aabcd9cf3013c57cb6a
andrewzhivolup <ferari1190@mail.ru>
2023-12-08 21:49:28 +0300

    feat: added timer gift

--- src/App.js
+++ src/App.js
@@ -5,18 +5,34 @@ import {SecondPage} from "./pages/2-page";
 import {ThirdPage} from "./pages/3-page";
 import {FourthPage} from "./pages/4-page";
 import {FifthPage} from "./pages/5-page";
+import {Gift} from "./widgets/Gift";
+import {useEffect, useState} from "react";
 
 function App() {
+    const birthday = new Date('12.23.2022 00:00').getTime();
+    const [needDate, setNeedDate] = useState(birthday - Date.now());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setNeedDate(birthday - Date.now());
+            if(needDate > 0) {
+                console.log(~~((birthday - Date.now())/1000/60));
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [])
+
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<FirstPage/>}/>
-                    <Route path="75489384" element={<SecondPage/>}/>
-                    <Route path="38109423" element={<ThirdPage/>}/>
-                    <Route path="42763541" element={<FourthPage/>}/>
-                    <Route path="13122019" element={<FifthPage/>}/>
-                    <Route path="85301395" element={<div>6</div>}/>
+                    <Route path="/" element={needDate > 0 ? <Gift/> : <FirstPage/>}/>
+                    <Route path="75489384" element={needDate > 0 ? <Gift/> : <SecondPage/>}/>
+                    <Route path="38109423" element={needDate > 0 ? <Gift/> : <ThirdPage/>}/>
+                    <Route path="42763541" element={needDate > 0 ? <Gift/> : <FourthPage/>}/>
+                    <Route path="13122019" element={needDate > 0 ? <Gift/> : <FifthPage/>}/>
+                    <Route path="85301395" element={needDate > 0 ? <Gift/> : <div>6</div>}/>
                     <Route path="*" element={<div>не существует </div>}/>
                 </Routes>
             </BrowserRouter>
