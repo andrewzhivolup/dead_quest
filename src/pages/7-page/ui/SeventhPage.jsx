704b8796e33fa16ce2cc1f5bbfe71b105c1bc4fc
andrewzhivolup <ferari1190@mail.ru>
2023-12-11 22:18:02 +0300

    fix: fixed 7 page

--- src/pages/7-page/ui/SeventhPage.jsx
+++ src/pages/7-page/ui/SeventhPage.jsx
@@ -11,8 +11,7 @@ const SeventhPage = () => {
                 <Title level={3}>Ладно, пора просто остановиться!</Title>
                 <Title level={3}>Поздравляю с днёй рождения.</Title>
                 <Title level={3}>Вот твой сладкий подарок:</Title>
-                <Title>P6H3K-A4ALR-0X4B8</Title>
-                <Paragraph copyable>This is a copyable text.</Paragraph>
+                <Paragraph copyable>P6H3K-A4ALR-0X4B8</Paragraph>
                 <Title level={4}>На самом деле это вторая попытка  создать что-то подобное</Title>
                 <Title level={4}>Если помнишь картину, там на другой стороне было кое-что зашифровано</Title>
                 <Title level={4}>Но там была настолько сложная загадка, что я сам запутался в какой-то момент</Title>
